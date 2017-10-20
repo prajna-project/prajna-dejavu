577ad08c8b06b9fc57f6223a418e576ab9d32b70
YoungLee <youngleemails@gmail.com>
2017-10-20 15:35:08 +0800

    semver up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-function dejavu(config: any): any {
-    return function () {
-        console.log('dejavu');
+import * as md5 from 'md5';
+
+let time: number = +new Date();
+
+function dejavu(ctx: any, next: any): any {
+    let episodeId = 'episode-' + md5(time + '');
+    let senceId = 'sence-' + md5(time + '');
+    ctx.episodeId = ctx.runtime.episodeId = episodeId;
+    ctx.senceId = ctx.runtime.senceId = senceId;
+
+    if (ctx.state.error) {
+        let new_time: number = +new Date();
+        let senceId = 'sence-' + md5(time + '');
+        ctx.senceId = ctx.runtime.senceId = senceId;
     }
+
+    ctx.core.record = function () {
+        // TODO:
+    };
+
+    ctx.core.finish = function () {
+        // TODO:
+    };
+
+    next();
 }
 
 export default dejavu;
