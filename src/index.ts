e54db5c2ffe4d0cb478514e46a6075929681121c
YoungLee <youngleemails@gmail.com>
2017-11-02 13:51:27 +0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,25 @@ import * as md5 from 'md5';
 
 let time: number = +new Date();
 let DEJAVU_FLAG: boolean = true;
+let episodeId = 'episode-' + md5(time + '');
+let senceId = 'sence-' + md5(time + '');
 
 function dejavu(ctx: any, next: any): any {
-    let episodeId = 'episode-' + md5(time + '');
-    let senceId = 'sence-' + md5(time + '');
-    ctx.episodeId = ctx.runtime.episodeId = episodeId;
-    ctx.senceId = ctx.runtime.senceId = senceId;
+    ctx.setKey('episodeId', episodeId);
+    ctx.setKey('senceId', senceId);
 
     if (DEJAVU_FLAG) {
         DEJAVU_FLAG = false;
         ctx.core.on('ERROR', function () {
             let new_time: number = +new Date();
-            let senceId = 'sence-' + md5(new_time + '');
-            ctx.senceId = ctx.runtime.senceId = senceId;
-            console.log('senceId is: ', ctx.senceId);
+            senceId = 'sence-' + md5(new_time + '');
+            ctx.setKey('senceId', senceId);
         });
 
         ctx.core.record = function () {
             let new_time: number = +new Date();
-            let senceId = 'sence-' + md5(new_time + '');
-            ctx.senceId = ctx.runtime.senceId = senceId;
+            senceId = 'sence-' + md5(new_time + '');
+            ctx.setKey('senceId', senceId);
 
             ctx.report({
                 level: 'INFO',
@@ -32,8 +31,8 @@ function dejavu(ctx: any, next: any): any {
 
         ctx.core.finish = function () {
             let new_time: number = +new Date();
-            let senceId = 'sence-' + md5(new_time + '');
-            ctx.senceId = ctx.runtime.senceId = senceId;
+            senceId = 'sence-' + md5(new_time + '');
+            ctx.setKey('senceId', senceId);
 
             ctx.report({
                 level: 'INFO',
