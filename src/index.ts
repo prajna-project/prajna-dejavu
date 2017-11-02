eeb86b77b1631e17dba0c4521acf360c5909f2c2
YoungLee <youngleemails@gmail.com>
2017-11-02 12:26:29 +0800

    on error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as md5 from 'md5';
 
 let time: number = +new Date();
+let DEJAVU_FLAG: boolean = true;
 
 function dejavu(ctx: any, next: any): any {
     let episodeId = 'episode-' + md5(time + '');
@@ -8,19 +9,39 @@ function dejavu(ctx: any, next: any): any {
     ctx.episodeId = ctx.runtime.episodeId = episodeId;
     ctx.senceId = ctx.runtime.senceId = senceId;
 
-    if (ctx.state.error) {
-        let new_time: number = +new Date();
-        let senceId = 'sence-' + md5(time + '');
-        ctx.senceId = ctx.runtime.senceId = senceId;
+    if (DEJAVU_FLAG) {
+        DEJAVU_FLAG = false;
+        ctx.core.on('ERROR', function () {
+            let new_time: number = +new Date();
+            let senceId = 'sence-' + md5(new_time + '');
+            ctx.senceId = ctx.runtime.senceId = senceId;
+            console.log('senceId is: ', ctx.senceId);
+        });
+
+        ctx.core.record = function () {
+            let new_time: number = +new Date();
+            let senceId = 'sence-' + md5(new_time + '');
+            ctx.senceId = ctx.runtime.senceId = senceId;
+
+            ctx.report({
+                level: 'INFO',
+                name: 'prajna-dejavu-record',
+                content: 'start recording'
+            });
+        };
+
+        ctx.core.finish = function () {
+            let new_time: number = +new Date();
+            let senceId = 'sence-' + md5(new_time + '');
+            ctx.senceId = ctx.runtime.senceId = senceId;
+
+            ctx.report({
+                level: 'INFO',
+                name: 'prajna-dejavu-finish',
+                content: 'finish recording'
+            });
+        };
     }
-
-    ctx.core.record = function () {
-        // TODO:
-    };
-
-    ctx.core.finish = function () {
-        // TODO:
-    };
 
     next();
 }
