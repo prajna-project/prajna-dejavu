18f4d2c7b0ec8e0c28c7629f1f1c918f33cc3b08
YoungLee <youngleemails@gmail.com>
2017-11-13 12:30:53 +0800

    full md5 message

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
 import * as md5 from 'md5';
 
-let time: number = +new Date();
+let ts: number = +new Date();
 let DEJAVU_FLAG: boolean = true;
-let episodeId = 'episode-' + md5(time + '');
-let senceId = 'sence-' + md5(time + '');
+let episodeId: string = md5('episode-' + ts + '');
+let senceId: string = md5('sence-' + ts + '');
 
 function dejavu(ctx: any, next: any): any {
-    ctx.setKey('episodeId', episodeId);
-    ctx.setKey('senceId', senceId);
+    ctx.defineGetter('episodeId', episodeId);
+    ctx.defineGetter('senceId', senceId);
 
     if (DEJAVU_FLAG) {
         DEJAVU_FLAG = false;
         ctx.core.on('ERROR', function () {
             let new_time: number = +new Date();
             senceId = 'sence-' + md5(new_time + '');
-            ctx.setKey('senceId', senceId);
+            ctx.defineGetter('senceId', senceId);
         });
 
         ctx.core.record = function () {
             let new_time: number = +new Date();
             senceId = 'sence-' + md5(new_time + '');
-            ctx.setKey('senceId', senceId);
+            ctx.defineGetter('senceId', senceId);
 
             ctx.report({
                 level: 'INFO',
@@ -32,7 +32,7 @@ function dejavu(ctx: any, next: any): any {
         ctx.core.finish = function () {
             let new_time: number = +new Date();
             senceId = 'sence-' + md5(new_time + '');
-            ctx.setKey('senceId', senceId);
+            ctx.defineGetter('senceId', senceId);
 
             ctx.report({
                 level: 'INFO',
